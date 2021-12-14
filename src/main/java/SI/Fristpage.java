1fb0b6a04ac3c551ce6ad56a508bac9e6d4a2802
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-12-15 00:35:01 +0530

    1st commit

---
+++ src/main/java/SI/Fristpage.java
@@ -0,0 +1,20 @@
+package SI;
+
+public class Fristpage {
+	void fristpage() {
+		for (int i = 0; i < 2; i++) {
+			System.out.println();
+		}
+		for (int i = 0; i < 30; i++) {
+			System.out.print("-");
+		}
+		System.out.print("Animal Database- An animal welfare System");
+		for (int i = 0; i < 43; i++) {
+			System.out.print("-");
+		}
+		for (int i = 0; i < 2; i++) {
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/main/java/SI/Main.java
@@ -0,0 +1,11 @@
+package SI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Fristpage obj = new Fristpage();
+		obj.fristpage();
+
+	}
+
+}
