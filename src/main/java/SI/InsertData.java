a8a49851f8bcedfde6370f20f55a2a41e8972814
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-12-16 23:31:34 +0530

    Insert Page completed

--- src/main/java/SI/InsertData.java
+++ src/main/java/SI/InsertData.java
@@ -11,16 +11,18 @@ public class InsertData {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/si", "root", "Khanna5301");
-			String g = "insert into Dog( S_No, NameofPerson, Location, MobileNo, CITY, PINCODE, STATE, Dogcolour, DogBreed) values( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			String g = "insert into Dog( unique_id, NameofPerson, Location, MobileNo, CITY, PINCODE, STATE, Dogcolour, DogBreed) values( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			PreparedStatement ptsmt = con.prepareStatement(g);
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("Enter ID :");
-			String S_No = br.readLine();
-			System.out.println("Enter Name:");
+			System.out.println();
+			int a = 1, b = 400;
+			int unique_id = (int) (Math.random() * (b - a + 1) + a);
+			// String S_No = br.readLine();
+			System.out.println("Enter your Name:");
 			String NameofPerson = br.readLine();
 			System.out.println("Enter the loaction:");
 			String Location = br.readLine();
-			System.out.println("Enter Mobile:");
+			System.out.println("Enter your Mobile Number:");
 			String MobileNo = br.readLine();
 			System.out.println("Enter City:");
 			String CITY = br.readLine();
@@ -32,17 +34,20 @@ public class InsertData {
 			String Dogcolour = br.readLine();
 			System.out.println("Enter DogBreed:");
 			String DogBreed = br.readLine();
-			ptsmt.setInt(1, Integer.parseInt(S_No));
+			ptsmt.setInt(1, unique_id);
 			ptsmt.setString(2, NameofPerson);
 			ptsmt.setString(3, Location);
-			ptsmt.setInt(4, Integer.parseInt(MobileNo));
+			ptsmt.setString(4, MobileNo);
 			ptsmt.setString(5, CITY);
 			ptsmt.setInt(6, Integer.parseInt(PINCODE));
 			ptsmt.setString(7, STATE);
 			ptsmt.setString(8, Dogcolour);
 			ptsmt.setString(9, DogBreed);
 			ptsmt.executeUpdate();
+			System.out.println();
 			System.out.println("Inserted Successfully...");
+			System.out.println();
+			System.out.println("Unique id for the dog is: " + unique_id);
 			con.close();
 		} catch (Exception e) {
 			e.printStackTrace();
