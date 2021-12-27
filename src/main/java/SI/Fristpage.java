1e37589553960856a88f772f208bedd448de9577
kartikeya532001 <60136050+kartikeya532001@users.noreply.github.com>
2021-12-27 23:04:00 +0530

    Deploy

--- src/main/java/SI/Fristpage.java
+++ src/main/java/SI/Fristpage.java
@@ -8,7 +8,7 @@ public class Fristpage {
 		for (int i = 0; i < 30; i++) {
 			System.out.print("-");
 		}
-		System.out.print("Animal Database- An animal welfare System");
+		System.out.print("Animal's Zone- A animal welfare System");
 		for (int i = 0; i < 43; i++) {
 			System.out.print("-");
 		}
